95b5078b2bc128598ae661f53c5e1eeb2fcc94e2
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-10-17 20:45:50 +0200

    Concatenate one file as the dates are stored already

--- src/domains/core.clj
+++ src/domains/core.clj
@@ -1,10 +1,16 @@
 (ns domains.core
   (:gen-class)
   (:use overtone.at-at)
-  (:require [domains.fetcher :as fetch]))
+  (:require [domains.fetcher :as fetch]
+            [clojure.java.io :refer [as-file]]))
 
 (def one-day-in-ms 86400000)
 (def at-at-pool (mk-pool))
+
+(defn read-existing-feed []
+  (if (.exists (as-file "domain-data.edn"))
+    (read-string (slurp "domain-data.edn"))
+    '()))
 
 (defn -main [& args]
   "Fetch the domains and write them as EDN once every n milliseconds
@@ -15,8 +21,11 @@
         curr-time (str (java.time.LocalDateTime/now))
         curr-date (first (clojure.string/split curr-time #"T"))]
     (every interval
-           #(do (spit (str "domain-data-" curr-date ".edn")
-                      (pr-str (fetch/run)) :append false)
+           #(do (spit "domain-data.edn"
+                      (pr-str
+                       (distinct
+                        (concat (read-existing-feed) (fetch/run))))
+                      :append false)
                 (println
                  (str curr-time
                       ": "
