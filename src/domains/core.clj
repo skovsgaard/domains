f5de42c1668e1d199ef485a87f1c329d69d991b2
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-10-23 16:20:39 +0200

    Move timer loop out of main

--- src/domains/core.clj
+++ src/domains/core.clj
@@ -12,22 +12,24 @@
     (read-string (slurp "domain-data.edn"))
     '()))
 
+(defn do-interval [interval]
+  (every interval
+         #(do
+            (spit "domain-data.edn"
+                  (pr-str
+                   (distinct
+                    (concat (read-existing-feed) (fetch/run))))
+                  :append false)
+            (println
+             (str (str (java.time.LocalDateTime/now))
+                  ": "
+                  "Fetched and saved domain expiry times.")))
+         at-at-pool))
+
 (defn -main [& args]
   "Fetch the domains and write them as EDN once every n milliseconds
   either from the first command line argument or from the default of once a day."
-  (let [interval (if (first args)
-                   (Integer/parseInt (first args))
-                   one-day-in-ms)
-        curr-time (str (java.time.LocalDateTime/now))
-        curr-date (first (clojure.string/split curr-time #"T"))]
-    (every interval
-           #(do (spit "domain-data.edn"
-                      (pr-str
-                       (distinct
-                        (concat (read-existing-feed) (fetch/run))))
-                      :append false)
-                (println
-                 (str curr-time
-                      ": "
-                      "Fetched and saved domain expiry times.")))
-           at-at-pool)))
+  (let [ms (if (first args)
+             (Integer/parseInt (first args))
+             one-day-in-ms)]
+    (do-interval ms)))
