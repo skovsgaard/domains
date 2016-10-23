a7dddb7ffb318ee81892b351973a5361969079a7
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-10-23 17:48:40 +0200

    Add proper argparsing

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Mozilla Public License"
             :url "https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [clj-http "2.3.0"]
                  [enlive "1.1.6"]
                  [overtone/at-at "1.2.0"]]
--- src/domains/core.clj
+++ src/domains/core.clj
@@ -2,10 +2,18 @@
   (:gen-class)
   (:use overtone.at-at)
   (:require [domains.fetcher :as fetch]
-            [clojure.java.io :refer [as-file]]))
+            [clojure.java.io :refer [as-file]]
+            [clojure.tools.cli :refer [parse-opts]]))
 
 (def one-day-in-ms 86400000)
 (def at-at-pool (mk-pool))
+
+(def cli-options
+  [["-i" "--interval MILLISECONDS" "Interval to poll in milliseconds"
+    :default one-day-in-ms
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 %)]]
+   ["-h" "--help" "Show this help text"]])
 
 (defn read-existing-feed []
   (if (.exists (as-file "domain-data.edn"))
@@ -29,7 +37,10 @@
 (defn -main [& args]
   "Fetch the domains and write them as EDN once every n milliseconds
   either from the first command line argument or from the default of once a day."
-  (let [ms (if (first args)
-             (Integer/parseInt (first args))
-             one-day-in-ms)]
-    (do-interval ms)))
+  (let [opts (parse-opts args cli-options)]
+    (if (:help (:options opts))
+      (println (str "Options:\n"
+                    (:summary opts)))
+      (do-interval (-> opts
+                       (get :options)
+                       (get :interval))))))
